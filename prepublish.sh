6568eec2007bdea623e13b8787a27e9fb3522b3d
Volkov Sergey <truefunrun@hotmail.com>
2019-04-23 21:55:04 +0300

    update travis config

--- prepublish.sh
+++ prepublish.sh
@@ -7,18 +7,18 @@ git remote add origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/stonex.gi
 git remote set-url origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/stonex.git
 git checkout -b ${PUBLISH_BRANCH}
 git symbolic-ref HEAD refs/heads/${PUBLISH_BRANCH}
-rm -rf example
+rm -rf example .release-it.json logo.png
 yarn build
 git add .
 git commit -m "build release ${PACKAGE_VERSION} [ci skip]"
 git push --set-upstream origin ${PUBLISH_BRANCH}
-{
-  yarn git-release ${PACKAGE_VERSION}
-  echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
-  npm whoami
-  npm publish
-  git checkout -- .
-  git push origin --delete ${PUBLISH_BRANCH}
-} || {
-  git push origin --delete ${PUBLISH_BRANCH}
-}
+# {
+#   yarn git-release ${PACKAGE_VERSION}
+#   echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+#   npm whoami
+#   npm publish
+#   git checkout -- .
+#   git push origin --delete ${PUBLISH_BRANCH}
+# } || {
+#   git push origin --delete ${PUBLISH_BRANCH}
+# }
