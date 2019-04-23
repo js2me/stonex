8c3e93aa8744a262366ea0b977e7baffe83f54d0
Volkov Sergey <truefunrun@hotmail.com>
2019-04-23 07:35:44 +0300

    chore: try to up travis ci (4)

---
+++ publish.sh
@@ -0,0 +1,13 @@
+ls -a
+echo It is worked!
+# rm -rf example
+# git remote rm origin
+# git remote add origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/stonex.git
+# git remote set-url origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/stonex.git
+# git fetch -p
+# git fetch
+# git symbolic-ref HEAD refs/heads/release
+# git symbolic-ref HEAD
+# git remote show origin
+# git status
+# yarn git-release ${PACKAGE_VERSION}
