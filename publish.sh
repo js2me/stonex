89661a23dd188d7c7f129b66fad4ccb6f241e975
Volkov Sergey <truefunrun@hotmail.com>
2019-04-23 10:59:19 +0300

    fix: endless trying to fix travis build. try (8)

--- publish.sh
+++ publish.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-ls -l
 set -e
+ls -l
 git remote rm origin
 git remote add origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/kinka.git
 git remote set-url origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/kinka.git
+git fetch -p
+git fetch
+git symbolic-ref HEAD refs/heads/${TRAVIS_BRANCH}
+git symbolic-ref HEAD
+git remote show origin
+git status
 rm -rf example
+yarn build
 git add .
 git commit -m "build release ${PACKAGE_VERSION} [ci skip]"
-git push origin HEAD:${TRAVIS_BRANCH}
+git push --set-upstream origin ${TRAVIS_BRANCH}
 yarn git-release ${PACKAGE_VERSION}
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 npm whoami
 npm publish
 git checkout -- .
-git push origin HEAD:${TRAVIS_BRANCH}
+git push --set-upstream origin ${TRAVIS_BRANCH}
 git reset --hard ${ACTUAL_DEV_VERSION}
-git push -f origin HEAD:${TRAVIS_BRANCH}
+git push -f origin ${TRAVIS_BRANCH}
