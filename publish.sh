85a5eb6b3feb98d68bbe6e851f610a409f05b250
Sergey Volkov <js2me@outlook.com>
2019-04-23 12:42:30 +0300

    try to fix publish travis bash script (1)

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ git remote add origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/stonex.gi
 git remote set-url origin https://js2me:${GITHUB_TOKEN}@github.com/acacode/stonex.git
 git fetch -p
 git fetch
-git symbolic-ref HEAD refs/heads/${TRAVIS_BRANCH}
+git symbolic-ref HEAD refs/heads/release
 git symbolic-ref HEAD
 git remote show origin
 rm -rf example
@@ -16,12 +16,12 @@ git add .
 echo "git status"
 git status
 git commit -m "build release ${PACKAGE_VERSION} [ci skip]"
-git push --set-upstream origin ${TRAVIS_BRANCH}
+git push --set-upstream origin release
 yarn git-release ${PACKAGE_VERSION}
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 npm whoami
 npm publish
 git checkout -- .
-git push --set-upstream origin ${TRAVIS_BRANCH}
+git push --set-upstream origin release
 git reset --hard ${ACTUAL_DEV_VERSION}
-git push -f origin ${TRAVIS_BRANCH}
+git push -f origin release
