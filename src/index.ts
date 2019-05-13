9dddc6f0d0d969f7adff966ab51a9511f9e3d4c1
Volkov Sergey <truefunrun@hotmail.com>
2019-05-14 02:02:24 +0300

    chore: prettify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-// ts disable
+/**
+ * Copyright (c) acacode, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
 import { StonexModule } from './StonexModule'
 import { StoreBinder } from './StoreBinder'
 export { default as StonexStore } from './StonexStore'
