7f429e50fbd25508ddd06e0b84fabcea3ee51610
Volkov Sergey <truefunrun@hotmail.com>
2019-05-25 00:55:01 +0300

    test: added more unit tests for StonexModule

--- tests/unit/StonexModule.spec.ts
+++ tests/unit/StonexModule.spec.ts
@@ -1,31 +1,55 @@
 import { StonexModule, StonexStore } from '../../src'
-// import { StonexModule } from '../../src/StonexModule'
 
-console.log('SttonexModule', StonexModule)
+class SpecModule extends StonexModule {
+  public state: any = {}
+
+  public updateSpecState (newData: any): void {
+    this.setState(newData)
+  }
+}
+
+class SpecNestedModule extends SpecModule {
+
+  public updateSpecState (newData: any): void {
+    this.setState(newData)
+  }
+}
+
+interface StonexModules {
+  specModule: SpecModule,
+  specNestedModule: SpecNestedModule,
+}
 
 const createSpecStore = () => {
-
-  class SpecModule extends StonexModule {
-    public state = {}
-
-    public updateSpecState (newData: any): void {
-      this.setState(newData)
-    }
-  }
-
-  return new StonexStore({
-    specModule: SpecModule
+  return new StonexStore<StonexModules>({
+    specModule: SpecModule,
+    specNestedModule: SpecNestedModule,
   })
 }
 
 describe('StonexModule', () => {
 
-  const specStore = createSpecStore()
+  let testableModule: SpecModule
+
+  beforeEach(() => {
+    testableModule = createSpecStore().modules.specModule
+  })
 
   describe('primitive specs', () => {
     test('module should be successfully connected to store', () => {
-      expect(specStore.modules.specModule).toBeDefined()
+      expect(testableModule).toBeDefined()
     })
+
+    // specStore.modules.specModule.updateSpecState()
+
+    // test('module should have ability to create state snapshot', () => {
+    //   expect(specStore.createStateSnapshot()).toStrictEqual({
+    //     specModule: {},
+    //     specNestedModule: {},
+    //   })
+    // })
+
+    // test('stonex store should have unique id')
 
     const requiredProperties = [
       'getState',
@@ -38,20 +62,62 @@ describe('StonexModule', () => {
 
     requiredProperties.forEach(property => {
       test(`module should contain require property ${property}`, () => {
-        expect(specStore.modules.specModule).toHaveProperty(property)
+        expect(testableModule).toHaveProperty(property)
       })
     })
 
-    // expect(specStore.modules.specModule).toHaveProperty('getStates')
   })
 
   describe('state property', () => {
 
     test('should have initial value', () => {
-      expect(specStore.modules.specModule.state).toStrictEqual({})
+      expect(testableModule.state).toStrictEqual({})
     })
 
-  })
-  // test()
+    test('should catch an exception when state has been reassigned manually (immutability 1 unit test)', () => {
+      let exceptionCatched = false
+      try {
+        testableModule.state = { 'foo': 'I want to change it' }
+      } catch (e) {
+        exceptionCatched = true
+      }
+      expect(exceptionCatched).toBeTruthy()
+      expect(testableModule.getState()).toStrictEqual({})
+    })
 
+    test('getState() should return actual state value (immutability 2 unit test)', () => {
+      testableModule.state.foo = 'bar'
+      expect(testableModule.getState()).toStrictEqual({})
+    })
+
+    test('getState() should return actual state value (immutability 3 unit test)', () => {
+      testableModule.state.foo = 'bar'
+      testableModule.setState(testableModule.state)
+      expect(testableModule.getState()).toStrictEqual({})
+    })
+
+    describe('updating state', () => {
+
+      // beforeEach(() => {
+      //   specStore
+      // })
+
+      describe('setState()', () => {
+
+        test('should update state (object -> object) (1)', () => {
+          testableModule.setState({ foo: 'bar' })
+          expect(testableModule.state).toStrictEqual({ foo: 'bar' })
+        })
+
+        test('should update state (object -> object) (2)', () => {
+          const newState = { ...testableModule.state }
+          newState.foo = 'baz'
+          testableModule.setState(newState)
+          expect(testableModule.state).toStrictEqual({ foo: 'baz' })
+        })
+
+      })
+
+    })
+  })
 })
