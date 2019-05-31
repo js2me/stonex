f6ea04dec2cede3e6266d58502e1baabac84477d
Sergey Volkov <js2me@outlook.com>
2019-05-31 15:25:46 +0300

    test: added unit tests for methods and properties of StoreBinder

--- tests/unit/StoreBinder.spec.ts
+++ tests/unit/StoreBinder.spec.ts
@@ -1,23 +1,32 @@
 import { createStoreBinder, StoreBinder } from '../../src'
-import { testPropertiesOnExist } from './__helpers__'
+import { testAllCases, testPropertiesOnExist } from './__helpers__'
 import { createSpecStore, StonexModules } from './__spec__'
 
 describe('StoreBinder', () => {
+  const moduleName = 'specModule'
   let testableStoreBinder: StoreBinder<any, StonexModules>
+  let specStore: any
 
   const initializeSpec = () => {
-    const specStore = createSpecStore()
-    testableStoreBinder = createStoreBinder('specModule', specStore)
+    specStore = createSpecStore()
+    testableStoreBinder = createStoreBinder(moduleName, specStore)
   }
 
   beforeEach(initializeSpec)
 
   describe('primitive specs', () => {
 
+    test('should be function', () => {
+      expect(typeof createStoreBinder).toBe('function')
+    })
+    test('should return object', () => {
+      expect(typeof createStoreBinder(moduleName, createSpecStore())).toBe('object')
+    })
+
     initializeSpec()
 
     testPropertiesOnExist([
-      ['setState', 'function'],
+      ['getState', 'function'],
       ['moduleName', 'string'],
       ['modules', 'object'],
       ['resetState', 'function'],
@@ -26,18 +35,56 @@ describe('StoreBinder', () => {
 
   })
 
-  // describe('properties', () => {
-  //   const properties: any = {
-  //   }
+  describe('properties', () => {
+    const properties: any = {
+      moduleName: [
+        ['should contains name of stonex module', () => {
+          expect(testableStoreBinder.moduleName).toBe(moduleName)
+        }],
+      ],
+      modules: [
+        ['should be the reference to store modules', () => {
+          expect(testableStoreBinder.modules).toBe(specStore.modules)
+        }],
+        ['should have ability to use some existing module', () => {
+          let exception = ''
+          try {
+            testableStoreBinder.modules.specNestedModule.addFoo()
+          } catch (e) {
+            exception = e
+          }
+          expect(exception).toBeFalsy()
+        }],
+      ],
+    }
 
-  //   testAllCases(properties, (property) => property)
-  // })
+    testAllCases(properties, (property) => property)
+  })
 
-  // describe('methods', () => {
-  //   const methods: any = {
-  //   }
+  describe('methods', () => {
+    const methods: any = {
+      getState: [
+        ['should return state of attached module', () => {
+          specStore.modules.specModule.addFoo()
+          expect(testableStoreBinder.getState()).toStrictEqual({ foo: 'bar' })
+        }],
+      ],
+      resetState: [
+        ['should reset state of attached module', () => {
+          specStore.modules.specModule.addFoo()
+          testableStoreBinder.resetState()
+          expect(testableStoreBinder.getState()).toStrictEqual({})
+        }],
+      ],
+      setState: [
+        ['should update state of attached module', () => {
+          testableStoreBinder.setState({ example: 'example' })
+          expect(testableStoreBinder.getState()).toStrictEqual({ example: 'example' })
+        }],
+      ],
+    }
 
-  //   testAllCases(methods, methodName => `${methodName}()`)
-  // })
+    testAllCases(methods, methodName => `${methodName}()`)
+  })
 
 })
